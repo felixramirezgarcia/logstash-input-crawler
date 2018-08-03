6d67b50d162aea4845056baa47360772065c40cf
felix R G <felixramirezgarcia@correo.ugr.es>
2018-08-03 13:17:46 +0200

    fixed

--- lib/logstash/inputs/crawler.rb
+++ lib/logstash/inputs/crawler.rb
@@ -2,11 +2,10 @@
 require "logstash/inputs/base"
 require "logstash/namespace"
 require "stud/interval"
-require "net/http"
+require "set"
 require "uri"
-require "mechanize"
-require "faraday"
-require "rss"
+require "nokogiri"
+require "open-uri"
 
 class LogStash::Inputs::Crawler < LogStash::Inputs::Base
   config_name "crawler"
@@ -17,88 +16,78 @@ class LogStash::Inputs::Crawler < LogStash::Inputs::Base
   # The message string to use in the event.
   config :url, :validate => :string, :required => true
 
-  #Set de interval for stoppable_sleep
-  config :interval, :validate => :number, :default => 86400
+  #Set de interval for stoppable_sleep 
+  config :interval, :validate => :number, :default => 200
 
   public
-   def register
-    @urls = []
-    @agent = Mechanize.new
-	  @agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	  @blacklist = ['http://fusion.google.com/','yahoo.com','live.com','netvibes.com']
-   end # def register
+  def register
+        @seen_pages = Set.new                      # Keep track of what we've seen
+  end # def register
 
 
-   def run(queue)
+  def run(queue)
     # we can abort the loop if stop? becomes true
     while !stop?
-      
-      page = @agent.get(@url)
-      page.links.each {|link|
-            if link.href.chars.last(3).join == "xml" && not_include_blacklist(link)
-              @urls << link.href
-            end
-      }
-      
-      links = @urls.uniq
 
-      links.each { |l|
-          response = Faraday.get l
-          handle_response(response, queue)
-      }
+        crawl_site(@url) do |page,uri|
+          event = LogStash::Event.new("link" => uri.to_s)
+	        decorate(event)
+       		queue << event
+        end
 
-    Stud.stoppable_sleep(@interval) { stop? }
+        evento = LogStash::Event.new("paginas_exploradas" => @seen_pages.length)
+	      decorate(evento)
+       	queue << evento
+
+      Stud.stoppable_sleep(@interval) { stop? }
     end # loop
-   end # def run
+  end # def run
 
 
-   def stop
+  def stop
     # nothing to do in this case so it is not necessary to define stop
     # examples of common "stop" tasks:
     #  * close sockets (unblocking blocking reads/accets)
     #  * cleanup temporary files
     #  * terminate spawned threads
-   end
+  end
 
-   def not_include_blacklist(link) 
-      for i in 0..@blacklist.length-1
-        if link.href.include?(@blacklist[i])
-          return false
+  def crawl_site( starting_at, &each_page )
+        files = %w[png jpeg jpg gif svg txt js css zip gz asp html htm PNG JPEG JPG GIF SVG TXT JS CSS ZIP GZ ASP HTML HTM]
+        starting_uri = URI.parse(starting_at)
+      
+        crawl_page = ->(page_uri) do              # A re-usable mini-function
+          unless @seen_pages.include?(page_uri)
+            @seen_pages << page_uri                # Record that we've seen this
+            begin
+              doc = Nokogiri.HTML(open(page_uri)) # Get the page
+              each_page.call(doc,page_uri)        # Yield page and URI to the block
+      
+              # Find all the links on the page
+              hrefs = doc.css('a[href]').map{ |a| a['href'] }
+
+              # Make these URIs, throwing out problem ones like mailto:
+              uris = hrefs.map{ |href| URI.join( page_uri, href ) rescue nil }.compact.uniq
+      
+              # Pare it down to only those pages that are on the same site
+              uris.select!{ |uri| uri.host == starting_uri.host }
+
+              # Throw out links to files (this could be more efficient with regex)
+              uris.reject!{ |uri| files.any?{ |ext| uri.path.end_with?(".#{ext}") } }
+      
+              # Remove #foo fragments so that sub-page links aren't differentiated
+              uris.each{ |uri| uri.fragment = nil }
+      
+              # Recursively crawl the child URIs
+              uris.each{ |uri| crawl_page.call(uri) }
+      
+            rescue OpenURI::HTTPError # Guard against 404s
+              warn "Skipping invalid link #{page_uri}"
+            end
+          end
         end
-      end
-      return true
-   end
-
-
-   def handle_response(response, queue)
-    body = response.body
-    begin
-      feed = RSS::Parser.parse(body)
-      feed.items.each do |item|
-        # Put each item into an event
-        @logger.debug("Item", :item => item.author)
-        handle_rss_response(queue, item)
-      end
-    rescue RSS::MissingTagError => e
-      @logger.error("Invalid RSS feed", :exception => e)
-    rescue => e
-      @logger.error("Uknown error while parsing the feed", :exception => e)
-    end
+        crawl_page.call( starting_uri )   # Kick it all off!
   end
-
-
-  def handle_rss_response(queue, item)
-    @codec.decode(item.description) do |event|
-      event.set("Feed",  @url)
-      event.set("published", item.pubDate)
-      event.set("title", item.title)
-      event.set("link", item.link)
-      event.set("author", item.author)
-      decorate(event)
-      queue << event
-    end
-  end
-
-
+      
 
 end # class LogStash::Inputs::Crawler
